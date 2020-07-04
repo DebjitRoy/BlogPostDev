29cb49a02e11bb380a747fb70f511cd750bbcdc2
Debjit Roy <debjitroy.dev@gmail.com>
2020-07-04 02:37:30 -0700

    preparation for prod push

--- server.js
+++ server.js
@@ -31,6 +31,14 @@ app.use("/api/posts", postsRoute);
 
 app.use(errorHandler);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(
   PORT,
   console.log(`server running in ${process.env.NODE_ENV} on ${PORT}`)
