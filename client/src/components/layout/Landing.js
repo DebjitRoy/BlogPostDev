a9b9202e2131308f8946fbf2a04ccb61afacad79
Debjit Roy <debjitroy.dev@gmail.com>
2020-07-16 14:32:57 -0700

    banner change

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -2,6 +2,12 @@ import React, { Fragment, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import CaroselLanding from "./CarouselLanding";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPenNib,
+  faMountain,
+  faBook,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Landing = () => {
   const [carosalState, changeCarousalState] = useState(null);
@@ -20,7 +26,7 @@ const Landing = () => {
   return (
     <Fragment>
       <section className="landing">
-        <div className="dark-overlay">
+        <div className="light-overlay">
           <div className="landing-inner">
             <h1 className="display-3 pt-5">বাতায়ন</h1>
             <p className="lead col-md-6">
@@ -36,21 +42,24 @@ const Landing = () => {
           <div className="row">
             <div className="col-md-4 mb-4 text-center">
               <Link className="menu-tile" to="/travel">
-                <i className="fas fa-road fa-3x mb-2"></i>
+                {/* <i className="fas fa-route fa-3x mb-2"></i> */}
+                <FontAwesomeIcon icon={faMountain} className="fa-3x mb-2" />
                 <h3>ভ্রমণিকা</h3>
                 <p>ভ্রমণ সংকলন। কাছে দূরে, দেশে বিদেশে, নানান অভিজ্ঞতা।</p>
               </Link>
             </div>
             <div className="col-md-4 mb-4 text-center">
               <Link className="menu-tile" to="/books">
-                <i className="fas fa-book fa-3x mb-2"></i>
+                {/* <i className="fas fa-book fa-3x mb-2"></i> */}
+                <FontAwesomeIcon icon={faBook} className="fa-3x mb-2" />
                 <h3>মনের আনন্দ</h3>
                 <p>বই পড়া, গান শোনা, সিনেমা দেখা, আর আড্ডার সাথে ধর্মচর্চাও।</p>
               </Link>
             </div>
             <div className="col-md-4 mb-4 text-center">
               <Link className="menu-tile" to="/miscl">
-                <i className="fab fa-affiliatetheme fa-3x mb-2"></i>
+                {/* <i className="fas fa-pen-fancy fa-3x mb-2"></i> */}
+                <FontAwesomeIcon icon={faPenNib} className="fa-3x mb-2" />
                 <h3>টুকিটাকি</h3>
                 <p>
                   সব রকম চিন্তা ভাবনা, ভালো লাগা, মন্দ লাগা; যা কিছু মনে আসে।
